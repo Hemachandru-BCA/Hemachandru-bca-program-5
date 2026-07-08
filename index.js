6e30447fe7ca602d8b3b10c35134ffc15caace02
Hemachandru <2006hemachandru@gmail.com>
2026-07-08 09:34:35 +0530

    Fix temperature conversion and clean up code

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-// Temperature Converter using map()
-
 const celsius = [0, 20, 30, 40];
 
-// TODO:
-// Convert the Celsius temperatures to Fahrenheit
-// Formula: (C × 9/5) + 32
-// Use the map() function and store the result in fahrenheit.
+let fahrenheit = celsius.map(temp => (temp * 9/5) + 32);
 
-let fahrenheit = celsius.map(temp => (temp * 9/5) + 32;
+console.log(`Celsius: ${celsius} \n Farenheit: ${fahrenheit}`);
 
-console.log(`Celsius: ${celsisus} \n Farenheit: ${fahrenheit}`);
-
-// Write your code below
-
-
-// Do not modify this line
 module.exports = fahrenheit;
